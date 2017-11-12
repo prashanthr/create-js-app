0b70382f9648528e88c5a94433a14edb0f7a2277
Prashanth <prashanth@slice.com>
2017-11-12 11:19:49 -0800

    update test module

--- test/index.js
+++ test/index.js
@@ -1,10 +1,23 @@
-import mocha from 'mocha'
+import { describe, it } from 'mocha'
 import chai from 'chai'
+import {
+  getSourcePath,
+  getTargetPath,
+  copyFiles,
+  updatePackageJson,
+  yarnInstall,
+  setDebugFlag,
+  logForce
+ } from '../lib/include/util'
+import _debug from 'debug'
+
+const debug = _debug('create-js-app-test')
 const should = chai.should()
 
 describe('create-js-app test suite', function () {
-  it('sample', function () {
-    (-1).should.equal([1, 2, 3].indexOf(4))
+  it('get-source-path', function () {
+    debug('source-path', getSourcePath())
+    return
   })
 })
 
--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,2 @@
  #!/usr/bin/env bash
-./node_modules/mocha/bin/mocha --require babel-core/register  --reporter landing ./test/index.js 
+./node_modules/mocha/bin/mocha --require babel-core/register --reporter landing ./test/index.js 
