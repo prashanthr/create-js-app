0d98a258ad0fffea37e9db97ee31af052be4be47
Prashanth <prashanth@slice.com>
2017-11-12 09:36:20 -0800

    feat(test): Add mocha,chai,tests,scripts and travis.yml

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import mocha from 'mocha'
+import chai from 'chai'
+const should = chai.should()
+
+describe('create-js-app test suite', function () {
+  it('sample', function () {
+    (-1).should.equal([1, 2, 3].indexOf(4))
+  })
+})
+
